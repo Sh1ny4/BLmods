b739cfa4659e074131c819fa42b59353361db1ae
Sh1ny4 <60614398+Sh1ny4@users.noreply.github.com>
2024-04-02 19:50:49 +0200

    update

--- wipo/patches/EliteInCastle/AiHourlyTickPatch.cs
+++ wipo/patches/EliteInCastle/AiHourlyTickPatch.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using TaleWorlds.CampaignSystem.CampaignBehaviors.AiBehaviors;
 using HarmonyLib;
+using TaleWorlds.CampaignSystem.CampaignBehaviors;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem;
 
 namespace wipo.patches.EliteInCastle
 {
+    [HarmonyPatch(typeof(RecruitmentCampaignBehavior), "UpdateVolunteersOfNotablesInSettlement")]
     internal class AiHourlyTickPatch : AiVisitSettlementBehavior
     {
+        [HarmonyPrefix]
+        static bool Prefix(ref AiVisitSettlementBehavior __instance, MobileParty mobileParty, PartyThinkParams p)
+        {
+            return false;
+        }
     }
 }
