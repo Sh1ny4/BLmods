a3631fdf04dfb9341d6973eb60af4009cf2798c4
Sh1ny4 <60614398+Sh1ny4@users.noreply.github.com>
2024-04-02 17:03:05 +0200

    update

---
+++ wipo/patches/Tweaks/UIsearchGetterPatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using HarmonyLib;
+using TaleWorlds;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.CharacterCreationContent;
+using TaleWorlds.CampaignSystem.ComponentInterfaces;
+using TaleWorlds.CampaignSystem.ViewModelCollection;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Inventory;
+
+namespace wipo.patches.Tweaks
+{
+    [HarmonyPatch(typeof(SPInventoryVM), "IsSearchAvailable", MethodType.Getter)]
+    internal class UIsearchGetterPatch
+    {
+        [HarmonyPrefix]
+        static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
