b087c950a1f018b59f05f09b19217e0b77749799
Sh1ny4 <60614398+Sh1ny4@users.noreply.github.com>
2024-04-07 20:09:27 +0200

    update

--- wipo/patches/Leveling/SkillLimitPatch.cs
+++ wipo/patches/Leveling/SkillLimitPatch.cs
@@ -12,11 +12,9 @@ namespace wipo.patches
         static void Postfix(ref ExplainedNumber __result, int attributeValue, int focusValue, TextObject attributeName, bool includeDescriptions = false)
         {
             ExplainedNumber result = new ExplainedNumber(0f, includeDescriptions, null);
-            result.Add((float)(focusValue * 60), skillFocusText, null);
+            result.Add((float)(focusValue * 60), new TextObject("{=MRktqZwu}Skill Focus", null), null);
             result.LimitMin(0f);
             __result = result;
         }
-
-        static TextObject skillFocusText = new TextObject("{=MRktqZwu}Skill Focus", null);
     }
 }
