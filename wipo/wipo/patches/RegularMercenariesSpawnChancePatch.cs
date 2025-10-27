23a81c4c7b178f650ee533594b67d669cd8c013c
Sh1ny4 <60614398+Sh1ny4@users.noreply.github.com>
2025-10-27 21:22:57 +0100

    Create RegularMercenariesSpawnChancePatch

---
+++ wipo/wipo/patches/RegularMercenariesSpawnChancePatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+
+namespace wipo.patches
+{
+    [HarmonyPatch(typeof(DefaultTavernMercenaryTroopsModel), nameof(DefaultTavernMercenaryTroopsModel.DefaultTavernMercenaryTroopsModel), MethodType.Getter)]
+    internal class IsSearchAvailablePatch
+    {
+        [HarmonyPostfix]
+        static void Postfix(ref float __result)
+        {
+            //remove the chance for caravan guards to spawn in taverns
+            __result = 1f;
+        }
+    }
+}
